2fee330ac050ce7e0ead112c81b58866e31d73f7
Alexandre <alexandre.ohara@hotmail.com>
2024-11-26 18:12:57 -0300

    add constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+const double spacing_4 = 4;
+const double spacing_8 = 8;
+const double spacing_16 = 16;
+const double spacing_24 = 24;
+const double spacing_32 = 32;
+const double spacing_40 = 40;
+const double spacing_48 = 48;
+
+const double borderRadius_4 = 4;
+const double borderRadius_8 = 8;
+const double borderRadius_16 = 16;
+const double borderRadius_24 = 24;
+const double borderRadius_32 = 32;
